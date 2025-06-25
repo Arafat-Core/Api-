f0aca5b5322589aeb6177870098bc5f47ffc9087
𝗔𝗿𝗮𝗳𝗮𝘁 𝘀𝗮𝗿𝗱𝗲𝗿 <arafatas206602@gmail.com>
2025-06-25 07:06:39 +0600

    Create quizzes.js

---
+++ quizzes.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    question: "বাংলাদেশের জাতীয় ফুল কী?",
+    options: ["রজনীগন্ধা", "গোলাপ", "শাপলা", "সুন্দরী"],
+    answer: "শাপলা"
+  },
+  {
+    question: "Naruto এর বাবার নাম কী?",
+    options: ["Minato", "Itachi", "Sasuke", "Kakashi"],
+    answer: "Minato"
+  },
+  {
+    question: "পানি কত ডিগ্রি সেলসিয়াসে ফুটে?",
+    options: ["50°C", "100°C", "0°C", "200°C"],
+    answer: "100°C"
+  },
+  {
+    question: "বাংলাদেশের রাজধানী কোনটি?",
+    options: ["চট্টগ্রাম", "রাজশাহী", "ঢাকা", "খুলনা"],
+    answer: "ঢাকা"
+  }
+];
