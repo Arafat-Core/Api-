7240f1db4efc7a3bd5acb2c6cc013a51956257ef
𝗔𝗿𝗮𝗳𝗮𝘁 𝘀𝗮𝗿𝗱𝗲𝗿 <arafatas206602@gmail.com>
2025-06-25 09:34:31 +0600

    Update quizzes.js

--- quizzes.js
+++ quizzes.js
@@ -18,5 +18,95 @@ module.exports = [
     question: "বাংলাদেশের রাজধানী কোনটি?",
     options: ["চট্টগ্রাম", "রাজশাহী", "ঢাকা", "খুলনা"],
     answer: "ঢাকা"
+  },
+  {
+    question: "Luffy এর ভাই কে?",
+    options: ["Ace", "Zoro", "Sanji", "Shanks"],
+    answer: "Ace"
+  },
+  {
+    question: "Einstein কোন সূত্রের জন্য বিখ্যাত?",
+    options: ["E=mc^2", "F=ma", "V=IR", "a^2 + b^2 = c^2"],
+    answer: "E=mc^2"
+  },
+  {
+    question: "One Piece এনিমেতে কে পাং গ্লিফ পড়তে পারে?",
+    options: ["Luffy", "Zoro", "Robin", "Nami"],
+    answer: "Robin"
+  },
+  {
+    question: "বাংলাদেশ স্বাধীন হয় কবে?",
+    options: ["২৬ মার্চ", "১৬ ডিসেম্বর", "২১ ফেব্রুয়ারি", "১৫ আগস্ট"],
+    answer: "১৬ ডিসেম্বর"
+  },
+  {
+    question: "সবচেয়ে বড় মহাদেশ কোনটি?",
+    options: ["আফ্রিকা", "এশিয়া", "ইউরোপ", "আমেরিকা"],
+    answer: "এশিয়া"
+  },
+  {
+    question: "চাঁদে প্রথম কে পা রেখেছিলেন?",
+    options: ["Neil Armstrong", "Buzz Aldrin", "Yuri Gagarin", "Michael Collins"],
+    answer: "Neil Armstrong"
+  },
+  {
+    question: "বাংলাদেশের জাতীয় পশু কোনটি?",
+    options: ["বাঘ", "হাতি", "গরু", "সিংহ"],
+    answer: "বাঘ"
+  },
+  {
+    question: "Dhaka University কবে প্রতিষ্ঠিত হয়?",
+    options: ["১৯২১", "১৯৪৭", "১৯৭১", "১৯৩০"],
+    answer: "১৯২১"
+  },
+  {
+    question: "আয়তনের দিক থেকে সবচেয়ে বড় দেশ কোনটি?",
+    options: ["চীন", "রাশিয়া", "কানাডা", "আমেরিকা"],
+    answer: "রাশিয়া"
+  },
+  {
+    question: "বাংলাদেশের জাতীয় সংগীতের রচয়িতা কে?",
+    options: ["কাজী নজরুল", "রবীন্দ্রনাথ ঠাকুর", "লালন ফকির", "আলাওল"],
+    answer: "রবীন্দ্রনাথ ঠাকুর"
+  },
+  {
+    question: "সূর্যগ্রহণ ঘটে কখন?",
+    options: ["সূর্য, পৃথিবী, চাঁদ", "পৃথিবী, চাঁদ, সূর্য", "চাঁদ, পৃথিবী, সূর্য", "সূর্য, চাঁদ, পৃথিবী"],
+    answer: "সূর্য, চাঁদ, পৃথিবী"
+  },
+  {
+    question: "Facebook কে প্রতিষ্ঠা করেন?",
+    options: ["Elon Musk", "Bill Gates", "Mark Zuckerberg", "Steve Jobs"],
+    answer: "Mark Zuckerberg"
+  },
+  {
+    question: "বিশ্বের সবচেয়ে লম্বা নদী কোনটি?",
+    options: ["আমাজন", "গঙ্গা", "নাইল", "যমুনা"],
+    answer: "নাইল"
+  },
+  {
+    question: "বায়ুমণ্ডলে সবচেয়ে বেশি গ্যাস কোনটি?",
+    options: ["অক্সিজেন", "হাইড্রোজেন", "নাইট্রোজেন", "কার্বন ডাইঅক্সাইড"],
+    answer: "নাইট্রোজেন"
+  },
+  {
+    question: "Dragon Ball Z তে Goku এর ছেলে কে?",
+    options: ["Trunks", "Gohan", "Vegeta", "Broly"],
+    answer: "Gohan"
+  },
+  {
+    question: "Google এর প্রতিষ্ঠাতা কারা?",
+    options: ["Bill Gates & Steve Jobs", "Larry Page & Sergey Brin", "Mark & Elon", "Sundar & Jeff"],
+    answer: "Larry Page & Sergey Brin"
+  },
+  {
+    question: "জাতিসংঘ দিবস কবে পালন করা হয়?",
+    options: ["২৪ অক্টোবর", "২৬ মার্চ", "১ জানুয়ারি", "৮ মার্চ"],
+    answer: "২৪ অক্টোবর"
+  },
+  {
+    question: "মানবদেহে মোট কতটি হাড় থাকে?",
+    options: ["২০৬", "৩০৬", "২৫৬", "১৯৬"],
+    answer: "২০৬"
   }
 ];
